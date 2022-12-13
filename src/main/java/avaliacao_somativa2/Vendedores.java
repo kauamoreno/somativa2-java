e8054d9f90720e82f804f9ce95baf377be8e9843
Kauã Moreno <kaua.moreno2005@gmail.com>
2022-12-13 20:15:19 -0300

    criando o array de vendedores

--- src/main/java/avaliacao_somativa2/Vendedores.java
+++ src/main/java/avaliacao_somativa2/Vendedores.java
@@ -2,4 +2,29 @@ package avaliacao_somativa2;
 
 public class Vendedores {
 
+	private String[] id = {"001","002","003"}; 
+	private String[] nomes = {"Jamil do Frete","Marco Antonio","Lucas Sidney"};
+	private double[] totalVendido = {0,0,0};
+	
+	/*****************GETTERS AND SETTERS*****************/
+	public String[] getId() {
+		return id;
+	}
+	public void setId(String[] id) {
+		this.id = id;
+	}
+	public String[] getNomes() {
+		return nomes;
+	}
+	public void setNomes(String[] nomes) {
+		this.nomes = nomes;
+	}
+	public double[] getTotalVendido() {
+		return totalVendido;
+	}
+	public void setTotalVendido(double[] totalVendido) {
+		this.totalVendido = totalVendido;
+	}
+	/*****************************************************/
+	
 }
